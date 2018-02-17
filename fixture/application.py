5bdd1ae657f911e95ceb51d542a7c57894d1a694
tsyganovskiy <tsyganovskiySS@gmail.com>
2018-02-17 22:25:03 +0200

    added session helper

--- fixture/application.py
+++ fixture/application.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-
+from fixture.session import SessionHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Chrome(executable_path='D:\\tools\\chromedriver.exe')
         self.wd.implicitly_wait(60)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click();
+        self.session = SessionHelper(self)
 
     def return_to_groups_page(self):
         wd = self.wd
@@ -34,17 +31,6 @@ class Application:
     def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]")
 
     def open_home_page(self):
         wd = self.wd
