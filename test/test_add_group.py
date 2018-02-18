70aa006bc2a50a0e11ee95c65406b5f1b62650b8
tsyganovskiy <tsyganovskiySS@gmail.com>
2018-02-18 19:21:05 +0200

    added intellectual verfication for login/logout

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="aaaaaaa", header="bbbbbbb", footer="ccccccc"))
 
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
