ef09cd84ffa9e17abcb67749a2bbfa131e6f3268
tsyganovskiy <tsyganovskiySS@gmail.com>
2018-02-24 22:49:50 +0200

    add navigation optimization

--- fixture/group.py
+++ fixture/group.py
@@ -32,7 +32,6 @@ class GroupHelper:
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-
         self.select_first_group()
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
@@ -53,4 +52,10 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not(wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new  ")) > 0):
+            wd.find_element_by_link_text("groups").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
