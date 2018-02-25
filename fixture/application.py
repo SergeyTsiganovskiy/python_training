70edf7e8966c10d226f7374b878ad7d8e37b22f9
tsyganovskiy <tsyganovskiySS@gmail.com>
2018-02-25 19:58:20 +0200

    add asserts to tests

--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.group import GroupHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Chrome(executable_path='D:\\tools\\chromedriver.exe')
-        self.wd.implicitly_wait(10)
+        self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,13 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="new name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="new header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
